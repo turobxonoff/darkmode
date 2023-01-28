9e269fcee790b1b6912b3fb024bb99bc496da0a4
Turobxonov Fayzulloxon <turobxonovfayzulloxon1@gmail.com>
2023-01-28 23:37:34 +0500

    Javascript dark mode

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('nav-switch').addEventListener('click', ()=> {
+        document.body.classList.toggle('dark');
+
+        localStorage.setItem('theme',
+           document.body.classList.contains('dark') ? 'dark' : 'light'
+        );
+    });
+
+    if(localStorage.getItem('theme' === "dark")) {
+        document.body.classList.add("dark");
+    }
+})
